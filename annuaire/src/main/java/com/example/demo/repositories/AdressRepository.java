5aa3f57fd360cf61d1d896c4358dff1aad847051
chebbi <chebbimohamed1987@gmail.com>
2018-03-29 21:36:32 +0200

    home add comments

--- annuaire/src/main/java/com/example/demo/repositories/AdressRepository.java
+++ annuaire/src/main/java/com/example/demo/repositories/AdressRepository.java
@@ -4,7 +4,7 @@ package com.example.demo.repositories;
 import com.example.demo.entities.Adress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//checkout home
 @Repository
 public interface AdressRepository extends JpaRepository<Adress, Long>{
 
